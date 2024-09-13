80f8fc36023e6a86804194b9a4b74cbc97eac5b2
Rahul Pillai <r.rahul17498@gmail.com>
2024-09-13 12:24:52 -0400

    Implement routing for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import LandingPage from './components/landingPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BattleGround from './components/battleGround';
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-     <LandingPage />
+      <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<LandingPage />}  />
+         <Route path='/battle' element={<BattleGround />}  />
+       </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
