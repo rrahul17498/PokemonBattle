b311a8386a8a7a802db21b26e29767dc310c2978
Rahul Pillai <r.rahul17498@gmail.com>
2024-09-17 11:19:15 -0400

    Project setup

---
+++ src/main/java/com/pokemonbattle/pokemonbattlebackend/PokemonbattlebackendApplication.java
@@ -0,0 +1,14 @@
+package com.pokemonbattle.pokemonbattlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonbattlebackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PokemonbattlebackendApplication.class, args);
+	}
+
+}
