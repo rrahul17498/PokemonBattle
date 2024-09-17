8c51a246ed6ed9f2ecdfa4719e1380fbb4b057e5
Rahul Pillai <r.rahul17498@gmail.com>
2024-09-17 17:43:28 -0400

    Add pokemon class

---
+++ src/main/java/com/pokemonbattle/pokemonbattlebackend/pokemon/Attack.java
@@ -0,0 +1,9 @@
+package com.pokemonbattle.pokemonbattlebackend.pokemon;
+
+public class Attack {
+    private String name;
+    private String mediaSrc;
+    private Integer power;
+    private Integer accuracy;
+    private Integer energyConsumed;
+}
---
+++ src/main/java/com/pokemonbattle/pokemonbattlebackend/pokemon/Pokemon.java
@@ -0,0 +1,25 @@
+package com.pokemonbattle.pokemonbattlebackend.pokemon;
+
+import java.util.List;
+
+public class Pokemon {
+    private Integer id;
+    private String name;
+    private Types type;
+    private List<Attack> attacks;
+    private Theme theme;
+
+
+
+    public enum Types {
+        FIRE, WATER, GRASS,
+    }
+
+    public record Theme(
+       String light,
+       String dark
+    ) {
+    }
+
+
+}
