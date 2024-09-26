50ab1229946192b3900be6cc086f1dbc097f8754
Rahul Pillai <r.rahul17498@gmail.com>
2024-09-26 19:22:51 -0400

    Remove redundant code

--- src/main/java/com/pokemonbattle/pokemonbattlebackend/PokemonbattlebackendApplication.java
+++ src/main/java/com/pokemonbattle/pokemonbattlebackend/PokemonbattlebackendApplication.java
@@ -1,14 +1,20 @@
 package com.pokemonbattle.pokemonbattlebackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PokemonbattlebackendApplication {
-
 	public static void main(String[] args) {
 
-		SpringApplication.run(PokemonbattlebackendApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(PokemonbattlebackendApplication.class, args);
+
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+		System.out.println(welcomeMessage.getWelcomeMessage());
 	}
 
 }
