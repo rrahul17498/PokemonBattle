ad3cd1cbb34fa3d9a4570a311a429daaacf5d72e
Rahul Pillai <r.rahul17498@gmail.com>
2024-10-09 19:38:09 -0400

    Add landing, register and login component layout

---
+++ frontend/src/features/users/components/login.tsx
@@ -0,0 +1,16 @@
+import { AuthLayout } from "@/components/layout/authLayout";
+
+
+export const Register = () => {
+
+    return (
+        <AuthLayout title={"Log in to your account"}>
+            <div>
+                Register Form
+            </div>
+        </AuthLayout>
+    );
+
+};
+
+ 
