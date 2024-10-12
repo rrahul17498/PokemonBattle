e94b568d7f55fb965fcc652b8a6dfb5d8774c507
Rahul Pillai <r.rahul17498@gmail.com>
2024-10-11 20:14:28 -0400

    [frontend] Minor refactor for auth and onboard components

--- frontend/src/features/onboard/index.tsx
+++ frontend/src/features/onboard/index.tsx
@@ -1,17 +1,17 @@
-import * as S from './styles';
 import ChooseYourPokemon from './choosePokemon';
+import { AuthLayout } from '@/components/layouts/authLayout';
 
-export const LandingPage = () => {
+export const OnBoard = () => {
 
-   return (
-   <S.Main>
-      <S.Section>
-        <S.LandingFormCard>
-          <S.LandingFormCardContent>
-            <ChooseYourPokemon />
-          </S.LandingFormCardContent>
-        </S.LandingFormCard>
-      </S.Section>
-   </S.Main>
-   );
+  return (
+    <main className="w-full h-full">
+    <section className="flex justify-center items-center h-full">
+      <div className="border border-border w-[1000px] p-6 rounded">
+        <div className="w-full">
+          <ChooseYourPokemon />
+        </div>
+      </div>
+    </section>
+  </main>
+  );
 };
--- frontend/src/features/users/onBoard.tsx
+++ frontend/src/features/users/onBoard.tsx
@@ -1,21 +1,18 @@
 import { AuthLayout } from '@/components/layouts/authLayout';
 import PokeballIcon from "@/assets/icons/pokeball_side_icon_1.png";
 import { Link } from '@/components/ui/link';
-
-
-const POKEMONS = ["Charmander", "Bulbasaur", "Squirtle"];
+import START_OFF_POKEMONS from "@/features/pokemon/startoffPokemons.json";
 
 
 export const OnBoard = () => {
 
   return (
     <AuthLayout hideLogo={true}>
-        {/* <ChooseYourPokemon /> */}
         <div>
             <h2 className="text-2xl font-semibold text-center mb-3">Choose Your Pokemon</h2>
             <div className="flex justify-between">
-                {POKEMONS.map(() => (
-                    <Link className="mx-8 cursor-pointer" to="/pokemon">
+                {START_OFF_POKEMONS.data.map((pokemonData) => (
+                    <Link className="mx-8 cursor-pointer" to={`/pokemon/${pokemonData.id}`}>
                         <img className="h-20 w-auto mx-auto mt-8 mb-4" src={PokeballIcon} alt="Pokeball" />
                     </Link>
                 ))}
