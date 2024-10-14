1ba7e28fb7d6e29588a7764fa0e9fc910c6d6e28
Rahul Pillai <r.rahul17498@gmail.com>
2024-10-14 14:55:32 -0400

    [frontend] adjust import statements in routing

--- frontend/src/app/routing/router.ts
+++ frontend/src/app/routing/router.ts
@@ -8,42 +8,42 @@ import AppRoutes from "./routes";
     {
         path: AppRoutes.landing,
         lazy: async() => {
-          const { LandingPage } = await import("../../features/landing");  
+          const { LandingPage } = await import("@/features/landing");  
           return { Component: LandingPage };
         }
     },
     {
         path: AppRoutes.guest,
         lazy: async() => {
-          const { Guest } = await import("../../features/users/guest");  
+          const { Guest } = await import("@/features/users/guest");  
           return { Component: Guest };
         }
     },
     {
       path: AppRoutes.onboard,
       lazy: async() => {
-        const { OnBoard } = await import("../../features/users/onBoard");  
+        const { OnBoard } = await import("@/features/users/onBoard");  
         return { Component: OnBoard };
       }
     },
     {
       path: AppRoutes.pokemon(":id"),
       lazy: async() => {
-        const { Pokemon } = await import("../../features/pokemon");  
+        const { Pokemon } = await import("@/features/pokemon");  
         return { Component: Pokemon };
       }
     },
     {
       path: AppRoutes.connectToBattle,
       lazy: async() => {
-        const { ConnectToBattle } = await import("../../features/battle/connectToBattle");  
+        const { ConnectToBattle } = await import("@/features/battle/connectToBattle");  
         return { Component: ConnectToBattle };
       }
     },
     {
       path: AppRoutes.battle(":id"),
       lazy: async() => {
-        const { Battle } = await import("../../features/battle");  
+        const { Battle } = await import("@/features/battle");  
         return { Component: Battle };
       }
     }
