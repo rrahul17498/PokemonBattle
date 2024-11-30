3d7bba755080af224ad51f343757a6266ad4822a
Rahul Pillai <r.rahul17498@gmail.com>
2024-11-29 22:08:15 -0500

    [backend] update PokemonState and BattleState fields, remove redundant code from battle connection handler

--- backend/src/main/java/com/pokemonbattle/pokemonbattlebackend/battle/battleState/PokemonState.java
+++ backend/src/main/java/com/pokemonbattle/pokemonbattlebackend/battle/battleState/PokemonState.java
@@ -6,9 +6,13 @@ import lombok.Data;
 @Data
 public class PokemonState {
      Long id;
-     Float health = 1.0F;
+     Integer health = 100;
 
     public PokemonState(Pokemon pokemon) {
         this.id = pokemon.getId();
+    }
+
+    public void receiveDamage(AttackDTO attackDTO) {
+        this.health = Math.max(0, Math.min(100, this.health - attackDTO.getPower()));
     }
 }
