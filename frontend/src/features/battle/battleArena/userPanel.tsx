dd47d7979e6178a81aefd10e7776764ae97df0fc
Rahul Pillai <r.rahul17498@gmail.com>
2024-12-15 04:21:25 -0500

    [frontend] Update battleResource/battleState/animation components refactor changes to userPanel and opponentPanel

--- frontend/src/features/battle/battleArena/userPanel.tsx
+++ frontend/src/features/battle/battleArena/userPanel.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import PokeballIcon from '@/assets/icons/pokeball_side_icon_1.png';
 import PokeballOpenIcon from '@/assets/icons/pokeball_open_1.png';
-import { PokemonDataType } from "../../pokemon/data/models";
-import { POKEMON_ACTION_TYPES, PokemonActionInput, PokemonStateType, USER_ACTION_TYPES, UserActionInput } from "../data/models";
+import { PokemonDataType, PokemonStatus } from "../../pokemon/data/models";
+import { PokemonActionTypes, PokemonActionInput, PokemonStateType, USER_ACTION_TYPES, UserActionInput } from "../data/models";
 import Button from "@/components/base/button";
 import { isNull } from "lodash";
 import PokemonHealthBar from "./pokemonHealthBar";
 import toast from "react-hot-toast";
+import { cn } from "@/utils/cn";
+import renderPokemonActionText from "./pokemonActionText";
 
 interface UserPanelProps {
     userId: number,
@@ -34,6 +36,7 @@ export const UserPanel = (
 
     const [chosenPokemonResource, setChosenPokemonResource] = useState<PokemonDataType | null>(null);
     const [chosenPokemonState, setChosenPokemonState] = useState<PokemonStateType | null>(null);
+    const [isActionInProgress, setIsActionInProgress] = useState<boolean>(false);
 
 
     useEffect(() => {
@@ -51,17 +54,18 @@ export const UserPanel = (
     }, [userId, ownedPokemons, chosenPokemonId]);
 
     useEffect(() => {
-        if (!chosenPokemonId) {
+        if (!chosenPokemonId || !pokemonsState) {
             return;
         }
 
-        const chosenPokemonState = pokemonsState.find((pokemonState) => pokemonState.id == chosenPokemonId);
-        if (!chosenPokemonState) {
+        const userChosenPokemonState = pokemonsState[chosenPokemonId];
+        if (!userChosenPokemonState) {
             console.error("Invalid pokemon id recieved: ", chosenPokemonId);
             toast.error("Invalid pokemon id"); 
             return;
         }
-        setChosenPokemonState(chosenPokemonState);
+        setChosenPokemonState(userChosenPokemonState);
+        setIsActionInProgress(false);
     }, [chosenPokemonId, pokemonsState]);
 
 
@@ -73,27 +77,33 @@ export const UserPanel = (
         return sendUserActionEvent({ type: USER_ACTION_TYPES.CHOOSE_POKEMON, pokemonId: pokemonId, playerId: userId }); 
     };
 
-    const onTriggerAttack = (pokemonId: number, attackId: number) => () => {
+    const onTriggerAttack = (pokemonId: number, attackId: number, attackName: string) => () => {
         if (sendPokemonActionEvent) {
-            return sendPokemonActionEvent({ type: POKEMON_ACTION_TYPES.ATTACK, sourceAttackId: attackId, sourcePlayerId: userId, sourcePokemonId: pokemonId, targetPokemonId  });
+            setIsActionInProgress(true);
+            sendPokemonActionEvent({ type: PokemonActionTypes.ATTACK, sourceAttackId: attackId, sourceAttackName: attackName, sourcePlayerId: userId, sourcePokemonId: pokemonId, targetPokemonId  });
+            return toast.custom(renderPokemonActionText(attackName), { position: "top-left", duration: 2000 });
+            
         }
     }
+
 
     return (
         <section className="border-border border flex flex-col justify-end">
         {!isNull(chosenPokemonResource)
-        ? <div className="mb-4 mt-12">
-            <PokemonHealthBar className="mb-24" pokemonState={chosenPokemonState} /> 
-            <img className="max-w-60 mx-auto animate-pokemon-render" src={chosenPokemonResource?.image} />
-            <h3 className="mt-3 p-3 font-semibold text-2xl">{chosenPokemonResource?.name}</h3>
-            <h4 className="px-3 text-lg font-medium mb-2">Moves</h4>
-            <ul className="flex flex-wrap p-3 min-h-20">
+        ? <div className="">
+            <img className={cn("max-w-60 mx-auto animate-pokemon-render", { "grayscale": chosenPokemonState?.status == PokemonStatus.DEFEATED})} src={chosenPokemonResource?.image} />
+            <h3 className="mt-3 px-3 font-semibold text-2xl">{chosenPokemonResource?.name}</h3>
+            <div className="pl-3 pr-6 py-4">
+                <PokemonHealthBar className="" pokemonState={chosenPokemonState} />
+            </div>
+            <h4 className="px-3 pb-3 text-lg font-medium">Moves</h4>
+            <ul className="flex flex-wrap px-3 min-h-20">
                 {chosenPokemonResource.attacks.map((attack, index) => (
                     <li key={`user_attack_${index}`} className="mx-2 my-1 list-none">
                         <Button
                         name={`user_trigger_attack_${index}`}
                         variant="small"
-                         onClick={onTriggerAttack(chosenPokemonId, attack.id)}
+                        onClick={!isActionInProgress ? onTriggerAttack(chosenPokemonId, attack.id, attack.name) : undefined}
                         >
                             {attack.name}
                         </Button>
@@ -103,7 +113,7 @@ export const UserPanel = (
         </div>
         : null}
         <div>
-            <h3 className="text-xl font-medium p-3">{userName}</h3>
+            <h3 className="text-xl font-medium px-3 mt-4">{userName}</h3>
             <ul className="grid grid-cols-3 p-3">
                  {
                     ownedPokemons.map((pokemon: PokemonDataType, i) => {
@@ -111,7 +121,7 @@ export const UserPanel = (
                         return (
                         <li key={`user_pokemon_${i}`}>
                             <button name={`pokeball_${i}`} onClick={toggleChosenPokemon(pokemon.id)}>
-                                <img className="h-12 mx-auto" src={isPokemonSelected ? PokeballOpenIcon : PokeballIcon} />
+                                <img className={cn("h-12 mx-auto", { "grayscale": chosenPokemonState?.status == PokemonStatus.DEFEATED })} src={isPokemonSelected ? PokeballOpenIcon : PokeballIcon} />
                                 <h5 className="text-center w-20 mt-2 text-sm">{pokemon.name}</h5>
                             </button>
                     </li>
