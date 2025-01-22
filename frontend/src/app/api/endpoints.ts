54df52813d9b66acc69f071f20c312c1f09bc699
Rahul Pillai <r.rahul17498@gmail.com>
2025-01-22 18:11:14 -0500

    [frontend] remove redundant files

--- frontend/src/app/api/endpoints.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const API_END_POINTS = {
-    user: {
-        createGuest: "/auth/register/guest",
-        create: "",
-        get: (id: number | string) => `/users/guest/${id}`,
-    },
-} as const;
